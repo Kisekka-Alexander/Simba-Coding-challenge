9aab13f408bd3b6cc93bc69aa82a3bf86fc2e07f
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-05-04 23:49:26 +0300

    Initial commit

---
+++ backend/todo/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Todo
+
+
+class ToDoAdmin(admin.ModelAdmin):
+    list = ('name', 'description', 'completed')
+
+
+admin.site.register(Todo, ToDoAdmin)
