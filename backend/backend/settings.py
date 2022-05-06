55249a51f0762b2008e2c58f2419aa1dfe8c5703
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-05-06 13:48:12 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -23,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'todo',
 ]
 
